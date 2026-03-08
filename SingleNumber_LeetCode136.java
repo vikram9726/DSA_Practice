3c3a377063976880a3bad0c6e170a9305df7f712
Vikram Varma <vikramvarma9726@gmail.com>
2026-03-08 14:41:43 +0530

    #

---
+++ SingleNumber_LeetCode136.java
@@ -0,0 +1,16 @@
+class SingleNumber_LeetCode136
+{
+    public static void main(String[] args)
+    {
+        int[] nums = {4,1,2,1,2};
+
+        int result = 0;
+
+        for(int num : nums)
+        {
+            result = result ^ num;    //Using XOR method :: 1^1=0 OR 1^0=1    
+        }
+
+        System.out.println("Single Number :: " + result);
+    }
+}
