58969b6d60d021792ebd27a84588f32474f6fa59
Vikram Varma <vikramvarma9726@gmail.com>
2026-03-11 22:18:38 +0530

    #

---
+++ SortColors_LeetCode75.java
@@ -0,0 +1,77 @@
+
+import java.util.Arrays;
+
+public class SortColors_LeetCode75 {
+
+    public static void main(String[] args) {
+
+        /*
+         * 
+         * Three Pointer Technique
+         * Dutch National Flag Algorithm
+         * 
+         * if nums[mid] == 0
+         * swap(nums[low], nums[mid])
+         * low++
+         * mid++
+         * 
+         * if nums[mid] == 1
+         * mid++
+         * 
+         * if nums[mid] == 2
+         * swap(nums[mid], nums[high])
+         * high--
+         * 
+         * 
+         */
+
+        int[] nums = { 2, 0, 2, 1, 1, 0 };
+
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+
+                high--;
+            }
+        }
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
+/*
+ * Basic initial approach
+ * 
+ * public class SortColors_LeetCode75 {
+ * public static void main(String[] args) {
+ * 
+ * int[] nums = {1,0,2,2,0,1};
+ * 
+ * Arrays.sort(nums);
+ * 
+ * for(int num : nums)
+ * {
+ * System.out.println(num);
+ * }
+ * 
+ * }
+ * 
+ * }
+ */
